a3020a3e331402ad26c0498abfc62b380728dfb6
xanderkiev2022 <301019a@gmail.com>
2023-01-15 07:05:26 +0200

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
 import user from './json/user.json';
 import data from './json/data.json';
+import friends from './json/friends.json';
 
 export const App = () => {
   return (
@@ -15,9 +17,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      
       <Statistics title="Upload stats" stats={data} />
-      {/* <Statistics stats={data} /> */}
+      <FriendList friends={friends} />;
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import FriendListItem from './FriendListItem/FriendListItem';
+
+export default function FriendList({ friends }) {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.exact({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
+  }),
+};
