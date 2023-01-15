657795a8cf85a232899e1f9b2777ad6b8423e134
xanderkiev2022 <301019a@gmail.com>
2023-01-15 07:33:33 +0200

    some changes

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
-import FriendListItem from './FriendListItem/FriendListItem';
 
 export default function FriendList({ friends }) {
   return (
@@ -20,9 +19,24 @@ export default function FriendList({ friends }) {
 
 FriendList.propTypes = {
   friends: PropTypes.exact({
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
     id: PropTypes.number.isRequired,
   }),
+};
+
+function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <li className={css.friendItem}>
+      <span className={isOnline ? `${css.isOnline}` : `${css.isOffline}`}>
+        {isOnline}
+      </span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
 };
