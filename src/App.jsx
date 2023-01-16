90a47e873ab1a89f600bd43f8487921bd5a3125f
Alex Koval <301019a@gmail.com>
2023-01-16 19:32:27 +0200

    space added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import data from './json/data.json';
 import friends from './json/friends.json';
 import transactions from './json/transactions.json';
 
+
 export const App = () => {
   return (
     <div>
